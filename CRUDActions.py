46b49001108b60d60f33b8e2dd5795ce4edbaa9c
josue <jcruz323@mail.ccsf.edu>
2026-02-14 09:08:50 -0800

    refactored: created handlers of actual arduino data & post handler after getting data, created templates in serialScriptMonitor.py to handle POST request

--- CRUDActions.py
+++ CRUDActions.py
@@ -1,7 +1,7 @@
 # purpose: handle the CRUD actions
 # to do: GET & POST
 
-from serialScriptMonitor import POSTPayLoadHandler # handles how the payload is made
+from serialScriptMonitor import POSTPayLoadHandler, getterSerialPort # handles how the payload is made
 
 # return type: dictionary
 def arduinoJSONHandler():
@@ -15,22 +15,29 @@ def arduinoJSONHandler():
 
     return informationCurrent # will be: what was scanned from arduino
 
+#handler's purpose: increase modularity and make method calls to keep track of sub processes
+
 # purpose: get current Arduino information in JSON format using pyserial lib.
+# return type: dictionary
 def getHandler():
-    return POSTPayLoadHandler() # get current information from arduino
+    # FIXME: this section will use pyserial to grab info from arduino w/ respective getter method
+    return getterSerialPort()
 
 # purpose: make the payload to client ab arduino info
 # argument one is what was given from getHandler()
 def postHandler(dataGrabbedArduino): # handles the full picture of post request sent to client
+    # datagrabbed from arduino is empty
+    if (dataGrabbedArduino == None):
+        return dict({"Status": "Arduino info lacking"})
+
+    payLoadInfo = None
     NGROKURLTOCLIENT = None #FIXME you need to grab FRONTEND SERVER URL from .env
     clientURL = NGROKURLTOCLIENT # Grab client endpoint to make POST requests to client server about arduino data
 
+    #now make a try-except for payload
+    try:
+        payLoadInfo = POSTPayLoadHandler(clientURL, dataGrabbedArduino)  # returns dict
+    except:
+        print("Couldn't make payload to client", payLoadInfo) # prints out the data of payLoadInfo
 
-    #FIXME: make secondary measure that POST request wasn't empty
-    # possible try-except block here for makePayload safety
-
-    #FIXME configure the payload from serialScriptMonitor.py's to make a POST request to client
-    # next line makes the POST request
-    # makePayload(clientURL, postJSONPayload) # make payload from getter to here
-
-    return "Test" # returns what was sent to client
+    return payLoadInfo # returns what was sent to client within middle man handling (return type: dictionary)
